8bbda323b1e6a45b4967fada0f9f3bca449f3bc2
Deni Saputra Hermawan <denisaputrah123@gmail.com>
2021-06-25 00:15:31 +0700

    commit folder
    pindahin folder qt designer ke local

--- Tes Dashboard/latihandataframe.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-df = pd.read_excel('masukan.xlsx')
-print(df.No.count())
-for x in range(4):
-    print(df.IPK[x])
--- Tes Dashboard/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-df_with_good_dates[df_with_good_dates.tier1 == 'planning'].groupby(
-    [df_with_good_dates.TGL.dt.year, df_with_good_dates.TGL.dt.month]).count().values.tolist()
